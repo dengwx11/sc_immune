a4b0cfa277a8164fd50dc2fe004ac225fd1fe5c0
dengwx11 <wenxuan.deng@yale.edu>
2020-08-17 21:50:01 -0400

    update argument format

--- SAME/Update_theta2.R
+++ SAME/Update_theta2.R
@@ -1,7 +1,10 @@
 # Update nuisance parameters, theta2
 set.seed(2020)
 
-update_tau_e <- function(Y0, W_tilde, alpha, w, z, alpha_e = alpha_prior_e, beta_e = beta_prior_e, N, D, Lambda){
+update_tau_e <- function(Y0, W_tilde, 
+                            alpha, w, z, 
+                            alpha_e = alpha_prior_e, beta_e = beta_prior_e, 
+                            N, D, Lambda){
     para1 <- rep(N/2 + alpha_e,D)
     para2 <- 0.5 * apply( Y0 - (alpha*w_tilde+(1-alpha)*w)%*%z, 1, sum) + beta_e
     tau_e_new <- matrix(0,nrow = Lambda, ncol = D)
