a7585c217a997802151862b2c1bc6570af89dc8b
dengwx11 <wenxuan.deng@yale.edu>
2020-08-17 22:44:09 -0400

    update

--- SAME/Update_theta2.R
+++ SAME/Update_theta2.R
@@ -8,7 +8,7 @@ update_tau_e <- function(Y0, W_tilde, # input
                             N, D # other para
                             ){                                
     para1 <- rep(N/2 + alpha_e,D)
-    para2 <- 0.5 * apply( Y0 - (alpha*w_tilde+(1-alpha)*w)%*%z, 1, sum) + beta_e
+    para2 <- 0.5 * apply( (Y0 - (alpha*w_tilde+(1-alpha)*w)%*%z)^2, 1, sum) + beta_e
     tau_e_new <- matrix(0,nrow = 1, ncol = D)
     for(d in 1:D){
         tau_e_new[,d] <- rgamma(1, para1[d],para2[d])
